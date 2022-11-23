16cb1a0f530d46917b333f5fc813ca84e7013c6c
Aleksandr Antciferov <wetterkrank@gmail.com>
2022-11-23 19:35:08 +0100

    add songs loading + styles

--- tomster-player/app/routes/albums/album.js
+++ tomster-player/app/routes/albums/album.js
@@ -1,7 +1,8 @@
 import Route from '@ember/routing/route';
 
 export default class AlbumRoute extends Route {
-  model(params) {
-    return this.store.findRecord('album', params.album_id);
-  }
+  // This is automatic:
+  // model(params) {
+  //   return this.store.findRecord('album', params.album_id);
+  // }
 }
--- tomster-player/app/routes/index.js
+++ tomster-player/app/routes/index.js
@@ -1,3 +1,7 @@
 import Route from '@ember/routing/route';
 
-export default class IndexRoute extends Route {}
+export default class IndexRoute extends Route {
+  beforeModel() {
+    this.replaceWith('albums');
+  }
+}
